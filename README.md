758cc6b6d3364afb82f5ad04ebe45338366e7243
yceffort <yceffort@gmail.com>
2020-09-28 18:21:20 +0900

    [Release] 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-chrome-extension-daum-en-ko-dictionary
+## 크롬 익스텐션 - 다음 영한사전
+
+찾고자 하는 단어를 더블클릭으로 선택해주세요. 단어의 뜻을 현재 마우스 위치에서 보여줍니다.
+
+![sample](sample.gif)
+
+## How to install
+
+1. https://github.com/yceffort/chrome-extension-daum-en-ko-dictionary/releases/ 에서 최신파일을 받아주세요.
+2. 주소창에 `chrome://extensions`을 입력하고 엔터를 눌러주세요.
+3. 우 상단의 개발자 모드를 활성화해주세요.
+4. 1에서 받은 파일을 압축 해제하고, `압축해제된 확장 프로그램을 로드합니다.`를 눌러서 해당 폴더를 선택해주세요.
--- content_script.js
+++ content_script.js
@@ -1,15 +1,39 @@
 "use strict";
 
-document.ondblclick = function () {
+document.ondblclick = function (e) {
   const selectedMessage = window.getSelection().toString();
 
   chrome.runtime.sendMessage({ word: selectedMessage }, async function (
     response
   ) {
     if (response.status === 200) {
-      console.log(response.body);
-    } else {
-      console.log(response);
+      const div = document.createElement("div");
+      div.classList.add("_daum_dict");
+      div.setAttribute("id", `_daum_dict`);
+
+      const ul = document.createElement("ul");
+
+      response.body.reverse().forEach((word) => {
+        const li = document.createElement("li");
+        li.textContent = word;
+        ul.appendChild(li);
+      });
+
+      div.style.zIndex = "999";
+      div.style.position = "absolute";
+      div.style.backgroundColor = "white";
+
+      const targetRect = e.target.getBoundingClientRect();
+
+      div.style.top = `${e.clientY + window.scrollY + e.target.style.height}px`;
+      div.style.left = `${e.clientX}px`;
+
+      div.onmouseleave = function (e) {
+        div.parentNode.removeChild(div);
+      };
+
+      div.appendChild(ul);
+      document.body.appendChild(div);
     }
   });
 };
